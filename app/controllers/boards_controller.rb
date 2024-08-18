7ce93db62c365b41b5d54c3e119641f268d35e48
長野汐央里 <>
2024-08-18 15:41:53 +0900

    ユーザーが複数のboardを作成できるようにした

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,6 +4,21 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @board = Board.new
+    @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :description)
   end
 end
