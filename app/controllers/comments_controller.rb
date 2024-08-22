848ce79ca8ccdabbd0e767c08dba3ca6543da35e
長野汐央里 <>
2024-08-22 19:18:37 +0900

    comment投稿フォームを作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,25 @@
 class CommentsController < ApplicationController
-  
+
   def new
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build
   end
 
   def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to board_task_path(@task.board, @task), notice: 'コメントを追加'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
