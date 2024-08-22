e4994b83b45727b0aa0fa81c661e4e4450300b41
長野汐央里 <>
2024-08-23 00:39:34 +0900

    タスク詳細ページでコメントも表示されるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,6 +25,7 @@ class TasksController < ApplicationController
   def show
     @board = Board.find(params[:board_id])
     @task = Task.find(params[:id])
+    @comments = @task.comments
   end
   
 
